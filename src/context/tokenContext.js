07c7192445ef31c3a24b95d0a2904f9cb00cbff3
Дмитрий <qf4wpo6jiwu7@list.ru>
2023-07-27 22:31:12 +0300

    ДЗ 1

--- src/context/tokenContext.js
+++ src/context/tokenContext.js
@@ -1,3 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {useToken} from '../hooks/useToken';
 
 export const tokenContext = React.createContext({});
+
+export const TokenContextProvider = ({children}) => {
+  const [token, delToken] = useToken('');
+  return (
+    <tokenContext.Provider value={{token, delToken}} >
+      {children}
+    </tokenContext.Provider>
+  );
+};
+
+TokenContextProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import {useAuth} from '../hooks/useAuth';
+
+
+// export const authContext = React.createContext({});
+
+// export const AuthContextProvider = ({children}) => {
+//   const [auth, clearAuth] = useAuth();
+
+//   return (
+//     <authContext.Provider value={{auth, clearAuth}}>
+//       {children}
+//     </authContext.Provider>
+//   );
+// };
+
+// AuthContextProvider.propTypes = {
+//   children: PropTypes.node.isRequired,
+// };
